974770b0520052c6cc26408c9fdd491a5b0b7b6f
Ahmed.BenEzzine <ahmedezzine338@gmail.com>
2022-11-17 13:46:17 +0100

    test10

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplTest.java
@@ -26,7 +26,7 @@
 	@ExtendWith(MockitoExtension.class)
 	public class ProduitServiceImplTest {
 	
-
+//Ahmed Ben Ezzine
 		
 		@Mock
 		ProduitRepository produitRepository;
@@ -44,7 +44,7 @@
 	        when(produitRepository.save(produit1)).thenReturn(produit1);
 	        assertNotNull(produit1);
 	        assertEquals(produit1, produitService.addProduit(produit1));
-	        log.info("add works !!");
+	        log.info("add works successfully");
 	    }
 	    @Test
 	    public void TestRetrieveAllProduits() {
@@ -52,27 +52,27 @@
 	                .of(produit1,produit2)
 	                .collect(Collectors.toList()));
 	        assertEquals(2,produitService.retrieveAllProduits().size());
-	        log.info("Retrieve produits works !");
+	        log.info("Retrieve produits works successfully");
 	    }
 	    @Test
 	    public void TestDeleteProduit() {
 	        produitRepository.save(produit1);
 	        produitService.deleteProduit(produit1.getIdProduit());
 	        verify(produitRepository, times(1)).deleteById(produit1.getIdProduit());
-	        log.info("Delete works !");
+	        log.info("Delete works successfully");
 	    }
 	    @Test
 	    public void TestUpdateProduit() {
 	        when(produitRepository.save(produit1)).thenReturn(produit1);
 	        assertNotNull(produit1);
 	        assertEquals(produit1, produitService.updateProduit(produit1));
-	        log.info("Update works !!");
+	        log.info("Update works successfully");
 	    }
 	    @Test
 	    public void TestRetrieveProduit() {
 	        when(produitRepository.findById(produit1.getIdProduit())).thenReturn(Optional.of(produit1));
 	        assertEquals(produit1, produitService.retrieveProduit(produit1.getIdProduit()));
-	        log.info("Retrieve works !!");
+	        log.info("Retrieve works successfully");
 	    }
 		
 	}
